4aee4f2c278ba8d4aeebb670a65035869b13bd58
mzgk <mzgkworks@gmail.com>
2018-01-29 10:56:27 +0900

    add: MapViewをアウトレット接続

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - アウトレット
+    @IBOutlet weak var mapView: MKMapView!
+
+    // MARK: - ライフサイクル
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
